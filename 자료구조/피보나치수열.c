a7e50f9560afbe92652cea079ad866b49f5d80ed
wlwodnr2 <64742982+wlwodnr2@users.noreply.github.com>
2021-04-20 11:11:27 +0900

    Create 피보나치수열.c

---
+++ 자료구조/피보나치수열.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//재귀함수
+int Fibo(int n) {
+    if (n == 1)
+        return 0;
+    else if (n == 2)
+        return 1;
+    else
+        return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main() {
+    Fibo(7);
+    return 0;
+}
