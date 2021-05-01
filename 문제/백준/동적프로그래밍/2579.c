923621f50419c05ce2857ef0196a4b52a993a96c
wlwodnr2 <64742982+wlwodnr2@users.noreply.github.com>
2021-05-01 15:55:30 +0900

    Update 2579.c

--- 문제/백준/동적프로그래밍/2579.c
+++ 문제/백준/동적프로그래밍/2579.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &N); //계단 개수
     for (int i = 0; i < N; i++) scanf("%d", &stair[i]); //계단 가중치 입력
     sum[0] = stair[0]; 
-    sum[1] = (stair[0]>stair[0] + stair[1])?stair[0]:(stair[0]+stair[1]); 
+    sum[1] = (stair[1]>stair[0] + stair[1])?stair[1]:(stair[0]+stair[1]); 
     sum[2] = (stair[1] + stair[2])> (stair[0] + stair[2])?(stair[1] + stair[2]):(stair[0] + stair[2]);
     for (int i = 3; i < N; i++) sum[i] = (sum[i - 3] + stair[i - 1] + stair[i]) > (sum[i - 2] + stair[i]) ? (sum[i - 3] + stair[i - 1] + stair[i]) : (sum[i - 2] + stair[i]);
     printf("%d", sum[N-1]);
