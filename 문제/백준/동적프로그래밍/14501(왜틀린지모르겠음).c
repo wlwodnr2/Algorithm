801f6e327d73bd576086f4534f073cc86016f9e4
wlwodnr2 <64742982+wlwodnr2@users.noreply.github.com>
2021-05-06 23:46:55 +0900

    Create 14501(왜틀린지모르겠음).c

---
+++ 문제/백준/동적프로그래밍/14501(왜틀린지모르겠음).c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+int main() {
+    int N, tm[16][2] = { 0, }, i, j; //16*2배열
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) scanf("%d %d", &tm[i][0], &tm[i][1]); //시간과 돈 입력
+    int sum[16] = { 0, };
+    if (N != 1) sum[1] = tm[1][1];
+    else if (N == 1 && tm[1][0] == 1) sum[1] = tm[1][1];
+    for (i = 2; i <= N; i++) {
+        j = i-1;
+        while (j>0) {
+            if (i >= (tm[j][0] + j)) sum[i] = sum[i] > sum[j] ? sum[i] : sum[j]; //앞에 이어질 수 있는 값 중 제일 큰 sum값을 넣어라
+            j--;
+        }
+        if (i + tm[i][0]-1 > N) sum[i] = 0; //만약 본인의 요일 + 걸리는 요일이 N을 넘어간다면 그건 0이다.
+        else sum[i] += tm[i][1]; //안 넘어간다면 자신의 값을 더해서 저장.
+    }
+    int max = sum[1];
+    for (i = 2; i <= N; i++) {
+        if (sum[i] > max) max = sum[i];
+    }
+    printf("%d\n", max);
+    return 0;
+}
