40c357a064599ccf58b79cde9dab41ad57610bf5
Ji Jae Ouk <64742982+wlwodnr2@users.noreply.github.com>
2021-07-23 16:51:09 +0900

    Create ArrayStack.h

---
+++ 자료구조/중위_후위표기법/ArrayStack.h
@@ -0,0 +1,24 @@
+#ifndef __AB_STACK_H__ //헤더 파일이 두 번 불리면 안 되기 때문에 에러 방지용 
+#define __AB_STACK_H__
+#define TRUE 1
+#define FALSE 0
+#define STACK_LEN 100
+
+typedef int Data; //그냥 정수형인데 typedef는 마치 #define같은 느낌 
+
+typedef struct _arrayStack
+{
+    Data stackArr[STACK_LEN];
+    int topIndex;
+}ArrayStack;
+
+typedef ArrayStack Stack;
+
+void StackInit(Stack* pstack); //스택 초기화
+int SIsEmpty(Stack* pstack); //스택 비었는가 ? 
+
+void SPush(Stack* pstack, Data data); // 스택에 push , 여기서 data는 그냥 int형 변수 
+Data SPop(Stack* pstack); //스택에 pop
+Data Speek(Stack* pstack); //스택에 peek 
+
+#endif
