c15998a5ee2243ec27f87d3f80a47d873749ce2f
Ji Jae Ouk <64742982+wlwodnr2@users.noreply.github.com>
2021-07-25 16:15:46 +0900

    Create BinaryTreeMain2.c

---
+++ 자료구조/이진트리순회구조2/BinaryTreeMain2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "BinaryTree2.h"
+
+void ShowIntData(int data);
+
+int main(void)
+{
+    //노드들 생성
+    BTreeNode* bt1 = MakeBTreeNode();
+    BTreeNode* bt2 = MakeBTreeNode();
+    BTreeNode* bt3 = MakeBTreeNode();
+    BTreeNode* bt4 = MakeBTreeNode();
+    BTreeNode* bt5 = MakeBTreeNode();
+    BTreeNode* bt6 = MakeBTreeNode();
+
+    SetData(bt1, 1);
+    SetData(bt2, 2);
+    SetData(bt3, 3);
+    SetData(bt4, 4);
+    SetData(bt5, 5);
+    SetData(bt6, 6);
+
+    MakeLeftSubTree(bt1, bt2); //bt1의 왼쪽 서브 트리가 bt2 
+    MakeRightSubTree(bt1, bt3); //bt1의 오른쪽 서브 트리가 bt3
+    MakeLeftSubTree(bt2, bt4); //bt2의 왼쪽 서브 트리가 bt4
+    MakeRightSubTree(bt2, bt5);
+    MakeLeftSubTree(bt3, bt6);
+
+    PreorderTraverse(bt1, ShowIntData);
+    printf("\n");
+    InorderTraverse(bt1, ShowIntData);
+    printf("\n");
+    PostorderTraverse(bt1, ShowIntData);
+    printf("\n");
+
+    return 0;
+}
+
+void ShowIntData(int data)
+{
+    printf("%d ", data);
+}
